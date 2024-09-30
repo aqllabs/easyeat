ec9a686bd31968b438129415a8b172195666e7f8
Gassyr <grasbaku@gmail.com>
2024-09-30 10:41:30 +0800

    Initial commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::factory(500)->create([
+            'created_at' => function () {
+                return now();
+            },
+        ]);
+
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
